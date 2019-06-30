cd8ba56f9826fb6687283519c3cdedffe0e3a09a
root <231284527@qq.com>
2019-06-30 09:09:51 +0800

    提交工程

---
+++ src/com/itheima/git/Class.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class Class {
+    public static void main(String[] args) {
+        System.out.println("wdsadasdad");
+        System.out.println("wdsadasdad");
+        System.out.println("wdsadasdad");
+        System.out.println("wdsadasdad");
+    }
+}
